5d639114b1ddeb9a5fa250fe0cbe0526c129243b
biletweb-macmini <bilet.igor@gmail.com>
2025-01-04 15:49:16 +0200

    fix

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,5 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/products/filter', [FilterController::class, 'filter']);
+Route::post('/category/products/', [FilterController::class, 'category']);
