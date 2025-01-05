6d6808e6c6135245a8eee70b000e838aa3a8e99e
biletweb-macmini <bilet.igor@gmail.com>
2025-01-05 20:20:15 +0200

    fix

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,6 @@ use App\Http\Controllers\FilterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/category/create', [CategoryController::class, 'createCategory']);
 Route::get('/categories/{id}/subcategories', [CategoryController::class, 'subcategories']);
 Route::get('/products/{id}/subcategories/filter', [FilterController::class, 'filter']);
